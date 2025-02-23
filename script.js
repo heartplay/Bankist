a5325a1571b145a46b9b6d78384db881f94b613b
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-02-23 15:33:47 +0500

    Refactoring and comments

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// Opening the registration window by clicking on one of the buttons
+btnsOpenModal.forEach((btn) => btn.addEventListener(`click`, openModal));
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+// Closing the registration window by clicking outside the window or on the close button
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+// Closing the registration window by pressing `ESC` button
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
+
+// Show registration account form function
+function openModal(e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// Hide register account form function
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
