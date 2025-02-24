b5c37d5c73573f813b4fcf7313828d6bcd811df9
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-02-24 21:40:33 +0500

    Implementing navigation to sections with event delegation

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScroll = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
 
+// Modal account registration window
 // Opening the registration window by clicking on one of the buttons
 btnsOpenModal.forEach((btn) => btn.addEventListener(`click`, openModal));
-
 // Closing the registration window by clicking outside the window or on the close button
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-
 // Closing the registration window by pressing `ESC` button
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
+    }
+});
+
+// "Learn more" smooth scroll button
+btnScroll.addEventListener(`click`, function (e) {
+    e.preventDefault(e);
+    section1.scrollIntoView({ behavior: `smooth` });
+});
+
+// Section navigation with event delegation
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains(`nav__link`) && !e.target.classList.contains(`btn--show-modal`)) {
+        const id = e.target.getAttribute(`href`);
+        document.querySelector(id).scrollIntoView({ behavior: `smooth` });
     }
 });
 
@@ -31,16 +47,3 @@ function closeModal() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
-
-// Smooth scroll testing
-const btnScroll = document.querySelector(`.btn--scroll-to`);
-const section1 = document.querySelector(`#section--1`);
-btnScroll.addEventListener(`click`, function (e) {
-    e.preventDefault(e);
-    const sect1Coords = section1.getBoundingClientRect();
-    window.scrollTo({
-        left: sect1Coords.left + window.pageXOffset,
-        top: sect1Coords.top + window.pageYOffset,
-        behavior: `smooth`,
-    });
-});
