af081dbf7626a8ba33fdfef75996f88e9fb697a6
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-02-25 16:11:32 +0500

    Links menu fade animation, refactoring, comments, small changes

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 'use strict';
 
+// Registration account window
 const modal = document.querySelector('.modal');
+// Overlay for UI with opened registration account window
 const overlay = document.querySelector('.overlay');
+// Close button for registration account window
 const btnCloseModal = document.querySelector('.btn--close-modal');
+// Open buttons for registration account window
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// Scroll button to first section
 const btnScroll = document.querySelector(`.btn--scroll-to`);
+// First section
 const section1 = document.querySelector(`#section--1`);
+// All tabs in second section
+const tabs = document.querySelectorAll(`.operations__tab`);
+// Tabs container for second section
+const tabsContainer = document.querySelector(`.operations__tab-container`);
+// All content for second section
+const tabsContent = document.querySelectorAll(`.operations__content`);
+// Navigation panel
+const nav = document.querySelector(`.nav`);
 
 // Modal account registration window
 // Opening the registration window by clicking on one of the buttons
 btnsOpenModal.forEach((btn) => btn.addEventListener(`click`, openModal));
 // Closing the registration window by clicking outside the window or on the close button
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', (e) => closeModal(e));
+overlay.addEventListener('click', (e) => closeModal(e));
 // Closing the registration window by pressing `ESC` button
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
+        closeModal(e);
     }
 });
 
@@ -35,11 +49,7 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
     }
 });
 
-// Tabs on section 2
-const tabs = document.querySelectorAll(`.operations__tab`);
-const tabsContainer = document.querySelector(`.operations__tab-container`);
-const tabsContent = document.querySelectorAll(`.operations__content`);
-
+// Tab navigation on second section
 tabsContainer.addEventListener(`click`, function (e) {
     e.preventDefault();
     // Select clicked tab
@@ -57,6 +67,12 @@ tabsContainer.addEventListener(`click`, function (e) {
         .classList.add(`operations__content--active`);
 });
 
+// Menu link fade animation
+nav.addEventListener(`mouseover`, handlerHoverOpacity.bind(0.5));
+nav.addEventListener(`mouseout`, handlerHoverOpacity.bind(1));
+
+// Functions
+
 // Show registration account form function
 function openModal(e) {
     e.preventDefault();
@@ -69,4 +85,23 @@ function closeModal(e) {
     e.preventDefault();
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
+}
+
+// Handler function for menu link fade animation
+function handlerHoverOpacity(e) {
+    e.preventDefault();
+    if (e.target.classList.contains(`nav__link`)) {
+        // Selecting hovered link
+        const link = e.target;
+        // Selecting all links except hovered
+        const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+        // Selecting logo
+        const logo = link.closest(`.nav`).querySelector(`img`);
+        // Making all links semitransparent except hovered
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = this;
+        });
+        // Making logo semitransparent
+        logo.style.opacity = this;
+    }
 }
